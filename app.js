4432477ddcd81b1e758de4bd5ec421a8efb01dd0
Swapnamoy3 <123042098+Swapnamoy3@users.noreply.github.com>
2023-07-24 00:11:31 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,5 @@
 
 
-//  let respanse=prompt("dou you want to be reaAlerted");
-
-   
-// if(respanse=="n"){
-//     console.log("OK");
-// }else{
-//     setTimeout((a) => {
-//         alert("Submit ypout HW"+a);
-//     }, 2000,1);
-// }
-
-// let id=setInterval(function(a,b) {
-//     console.log(a,b);
-//     b+=a;
-// }, 1000,1,1);
-
-// let elements=document.getElementsByTagName("a")
-// elements=Array.from(elements);
-// let arrayofelements=[];
-// for(let i=0;i<elements.length;i++){
-//   arrayofelements.push(elements[i].href)
-// }
-
-// (arrayofelements).sort;
-
-// for(let i=0;i<arrayofelements.length;i++){
-//     for(let j=0;j<elements.length;j++){
-//         if(elements[j].href==arrayofelements[i])
-//         console.log(elements[i].innerHTML)
-//     }
-//   }
-
-// let a=document.getElementById('bulb');
-// function glow(){
-//     a.classList.toggle('glow');
-// }
-// // a.addEventListener('click',glow);
-// setInterval(glow, 100);
-
 
 
 function timedispaly(){
@@ -47,7 +8,7 @@ function timedispaly(){
     let time=new Date();
 let hour=time.getHours();
 let amOrPm=Math.floor(hour/12);//0 if am and 1 if pm;
-hour=hour%12;
+hour=(hour==0)? 12 :hour%12;
 
 let deg=(hour/12)*360
 console.log(hour,amOrPm)
